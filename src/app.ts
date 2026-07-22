f5c558d67d78bd7f5c2e753ea2b2c7cc7bbe2987
MoniruzzamanBillal <mdmoniruzzamanbillal@gmail.com>
2026-07-23 05:59:37 +0600

    feat:live url add

--- src/app.ts
+++ src/app.ts
@@ -22,6 +22,7 @@ app.use(
       "https://devmats.vercel.app",
       "https://dev-mats.vercel.app",
       "http://localhost:8081",
+      "https://bikelog-web.vercel.app",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
